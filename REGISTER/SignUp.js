8cbe77f68ccdd6742788847ea672057a116a7ac3
Shinon <133506832+Shinonval@users.noreply.github.com>
2023-07-22 21:04:17 +0700

    update

--- REGISTER/SignUp.js
+++ REGISTER/SignUp.js
@@ -1,19 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
     const registerForm = document.getElementById("register-form");
-
+  
     registerForm?.addEventListener("submit", function (event) {
-        event.preventDefault();
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
-        const confirmPassword = document.getElementById("confirm-password").value;
+      // Ngăn hành động submit mặc định của form (reload hoặc chuyển trang tùy theo `action` của form)
+      event.preventDefault();
+  
+      // Lấy giá trị của các input
+      const username = document.getElementById("username").value;
+      const password = document.getElementById("password").value;
+      const confirmPassword = document.getElementById("confirm-password").value;
+  
+      // Kiểm tra password và confirm password có giống nhau hay không
+      if (password !== confirmPassword) {
+        alert("Mật khẩu không khớp");
+        return;
+      }
+  
+      // Lưu thông tin đăng ký vào Local Storage
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
 
-        if (password !== confirmPassword) {
-            alert("Mật khẩu không khớp");
-            return;
-        }
-
-        localStorage.setItem("username", username);
-        localStorage.setItem("password", password);
-        alert("Đã tạo thành công")
+      alert("Đăng ký tài khoản thành công.")
     });
-});
+  });

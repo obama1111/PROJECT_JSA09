09f271bd1e7ac1d9e1a07209370bf64eeedaf7fd
obama1111 <phucsang324@gmail.com>
2023-07-28 23:53:16 +0700

    theme done

--- MAIN/Main.js
+++ MAIN/Main.js
@@ -12,8 +12,8 @@ function hideSlide() {
 
 function nextSlide() {
     hideSlide();
-    currentSlide = (currentSlide + 1) % 
-    slides.length;
+    currentSlide = (currentSlide + 1) %
+        slides.length;
     showSlide();
 }
 
@@ -23,7 +23,7 @@ function prevSlide() {
     showSlide();
 }
 
-setInterval(nextSlide ,10000);
+setInterval(nextSlide, 10000);
 
 const nextbtn = document.getElementById("nextButton");
 nextbtn.addEventListener("click", () => {
@@ -32,40 +32,68 @@ nextbtn.addEventListener("click", () => {
 })
 
 const themeBG = document.getElementById("theme-icon");
+const NewIcon = "/IMGS/3.png"
+const LastIcon = "/IMGS/2.png"
+let themeOn = false;
+
+function Darktheme() {
+    const body = document.getElementsByTagName("body")[0];
+    body.style.backgroundColor = "#212F3D";
+    const text_boxes = document.getElementsByClassName("nav-tabs");
+    for (const textBox of text_boxes) {
+        textBox.style.color = "#FFFFFF";
+    }
+    themeBG.style.backgroundColor = "yellow"
+    themeBG.style.borderRadius = "2em"
+    themeBG.src = NewIcon;
+    themeBG.alt = "newicon"
+
+}
+
+function Lighttheme() {
+    const body = document.getElementsByTagName("body")[0];
+    body.style.backgroundColor = "white";
+    const text_boxes = document.getElementsByClassName("nav-tabs");
+    for (const textBox of text_boxes) {
+        textBox.style.color = "black";
+    }
+    themeBG.style.backgroundColor = "#212F3D"
+    themeBG.style.borderRadius = "2em"
+    themeBG.src = LastIcon;
+    themeBG.alt = "lasticon"
+}
+
 themeBG.addEventListener("click", () => {
-    const body = document.getElementById("body")
-    body.style.backgroundColor = "#321D18"
-    const text_box = document.getElementsByClassName("nav-tabs");
-    text_box.style.color = "#FFFFFF"
-    
+    if (themeOn === false) {
+        Darktheme();
+        themeOn = true;
+    } else {
+        Lighttheme();
+        themeOn = false;
+    }
+
+    const nextBtn = document.getElementById("nextButton");
+    nextBtn.addEventListener("click", nextSlide);
+
+    const prevBtn = document.getElementById("prevButton");
+    prevBtn.addEventListener("click", prevSlide);
+
+    showSlide();
 })
-// const nextbtn = document.getElementById("nextButton");
-// nextbtn.addEventListener("click", () => {
-//     nextSlide()
+// Click để mở phim trong slide show
+// const Bannermovie1 = document.getElementById("movie1")
+// Bannermovie1.addEventListener("click", () => {
+//     // window.location.href="https://phimmoichilld.net/xem/quy-quyet-4-tap-full-pm497"
+//     console.log("movie1");
 // })
 
-const nextBtn = document.getElementById("nextButton");
-nextBtn.addEventListener("click", nextSlide);
-
-const prevBtn = document.getElementById("prevButton");
-prevBtn.addEventListener("click", prevSlide);
-
-showSlide();
-
-
-const Bannermovie1 = document.getElementById("movie1")
-Bannermovie1.addEventListener("click", () => {
-    // window.location.href="https://phimmoichilld.net/xem/quy-quyet-4-tap-full-pm497"
-    console.log("movie1");
-})
-
-const Bannermovie2 = document.getElementById("movie2")
-Bannermovie2.addEventListener("click", () => {
-    console.log("movie2");
-    // window.location.href="https://phimmoichilld.net/info/doctor-strange-in-the-multiverse-of-madness-2022-pm10159"
-})
-const Bannermovie3 = document.getElementById("movie3")
-Bannermovie3.addEventListener("click", () => {
-    console.log("movie3");
-    // window.location.href="https://phimmoiyyy.net/phim-le/transformers-quai-thu-troi-day"
-})
+// const Bannermovie2 = document.getElementById("movie2")
+// Bannermovie2.addEventListener("click", () => {
+//     console.log("movie2");
+//     // window.location.href="https://phimmoichilld.net/info/doctor-strange-in-the-multiverse-of-madness-2022-pm10159"
+// })
+// const Bannermovie3 = document.getElementById("movie3")
+// Bannermovie3.addEventListener("click", () => {
+//     console.log("movie3");
+//     // window.location.href="https://phimmoiyyy.net/phim-le/transformers-quai-thu-troi-day"
+// }

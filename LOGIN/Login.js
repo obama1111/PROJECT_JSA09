c6aaf6bfe8f6fcee1595ed712e6d890d75268a37
Shinon <133506832+Shinonval@users.noreply.github.com>
2023-07-29 20:07:12 +0700

    fix

--- LOGIN/Login.js
+++ LOGIN/Login.js
@@ -33,42 +33,12 @@ document.addEventListener("DOMContentLoaded", function () {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
     const email = document.getElementById("email").value;
-    // Lấy thông tin đăng ký từ Local Storage
-    const registeredUsername = localStorage.getItem("username");
-    const registeredPassword = localStorage.getItem("password");
-    const registeredEmail = localStorage.getItem("email")
-
-    // Kiểm tra username và password có khớp với thông tin đăng ký hay không
-    if (username !== registeredUsername || password !== registeredPassword || email !== registeredEmail) {
-      const destroy = document.getElementById("login-form");
-      destroy.innerHTML = "";
-      const createH1 = document.getElementById("text_success");
-      createH1.insertAdjacentHTML("afterbegin", "<h3>Tài khoản không tồn tại</h3>");
-      createH1.style.color = "red"
-
-      const gap = document.getElementById("gap-btn")
-      gap.style.display = "flex";
-      gap.style.justifyContent = "space-between"
-      const back = document.getElementById("back_btn");
-      back.style.marginTop = "1em";
-      back.addEventListener("click", () => {
-        window.location.href = "/LOGIN/Login.html";
-      });
-      back.insertAdjacentHTML("afterbegin", '<button type="button">Thử lại</button>');
-
-      const next = document.getElementById("next_btn");
-      next.style.marginTop = "1em";
-      next.addEventListener("click", () => {
-        window.location.href = "/REGISTER/SignUp.html";
-      });
-      next.insertAdjacentHTML("beforeend", '<button type="button">Đăng ký</button>');
-      return;
-    }
 
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
+        console.log(user);
         // ...
       })
       .catch((error) => {
@@ -76,18 +46,53 @@ document.addEventListener("DOMContentLoaded", function () {
         const errorMessage = error.message;
       });
 
-    // Thông báo đăng nhập thành công và lưu thông tin đăng nhập vào Local Storage
-    localStorage.setItem("isLogin", true);
-    const destroy = document.getElementById("login-form");
-    destroy.innerHTML = "";
-    const createH1 = document.getElementById("text_success");
-    createH1.insertAdjacentHTML("afterbegin", "<h3>Đăng nhập thành công</h3>");
-    createH1.style.color = "Green";
-    const next = document.getElementById("next_btn");
-    next.style.marginTop = "1em";
-    next.addEventListener("click", () => {
-      window.location.href = "/MAIN/Main.html";
-    });
-    next.insertAdjacentHTML("beforeend", '<button type="button">NEXT</button>');
+
+    // // Lấy thông tin đăng ký từ Local Storage
+    // const registeredUsername = localStorage.getItem("username");
+    // const registeredPassword = localStorage.getItem("password");
+    // const registeredEmail = localStorage.getItem("email")
+
+    // // Kiểm tra username và password có khớp với thông tin đăng ký hay không
+    // if (username !== registeredUsername || password !== registeredPassword || email !== registeredEmail) {
+    //   const destroy = document.getElementById("login-form");
+    //   destroy.innerHTML = "";
+    //   const createH1 = document.getElementById("text_success");
+    //   createH1.insertAdjacentHTML("afterbegin", "<h3>Tài khoản không tồn tại</h3>");
+    //   createH1.style.color = "red"
+
+    //   const gap = document.getElementById("gap-btn")
+    //   gap.style.display = "flex";
+    //   gap.style.justifyContent = "space-between"
+    //   const back = document.getElementById("back_btn");
+    //   back.style.marginTop = "1em";
+    //   back.addEventListener("click", () => {
+    //     window.location.href = "/LOGIN/Login.html";
+    //   });
+    //   back.insertAdjacentHTML("afterbegin", '<button type="button">Thử lại</button>');
+
+    //   const next = document.getElementById("next_btn");
+    //   next.style.marginTop = "1em";
+    //   next.addEventListener("click", () => {
+    //     window.location.href = "/REGISTER/SignUp.html";
+    //   });
+    //   next.insertAdjacentHTML("beforeend", '<button type="button">Đăng ký</button>');
+    //   return;
+    // }
+
+    
+
+    // // Thông báo đăng nhập thành công và lưu thông tin đăng nhập vào Local Storage
+    // localStorage.setItem("isLogin", true);
+    // const destroy = document.getElementById("login-form");
+    // destroy.innerHTML = "";
+    // const createH1 = document.getElementById("text_success");
+    // createH1.insertAdjacentHTML("afterbegin", "<h3>Đăng nhập thành công</h3>");
+    // createH1.style.color = "Green";
+    // const next = document.getElementById("next_btn");
+    // next.style.marginTop = "1em";
+    // next.addEventListener("click", () => {
+    //   window.location.href = "/MAIN/Main.html";
+    // });
+    // next.insertAdjacentHTML("beforeend", '<button type="button">NEXT</button>');
   });
 });

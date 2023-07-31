59953e969c476c146c8094d8254871ebf37638bc
Shinon <133506832+Shinonval@users.noreply.github.com>
2023-07-31 22:02:44 +0700

    upload folder

---
+++ MOVIES/Insidious4/insidious4.js
@@ -0,0 +1,41 @@
+const themeBG = document.getElementById("theme-icon");
+const NewIcon = "/IMGS/3.png"
+const LastIcon = "/IMGS/2.png"
+let themeOn = false;
+
+function Darktheme() {
+    const body = document.getElementsByTagName("body")[0];
+    body.style.backgroundColor = "#212F3D";
+    const text_boxes = document.getElementsByClassName("nav-tabs");
+    for (const textBox of text_boxes) {
+        textBox.style.color = "#FFFFFF";
+    }
+    themeBG.style.backgroundColor = "yellow"
+    themeBG.style.borderRadius = "2em"
+    themeBG.src = NewIcon;
+    themeBG.alt = "newicon"
+
+}
+
+function Lighttheme() {
+    const body = document.getElementsByTagName("body")[0];
+    body.style.backgroundColor = "white";
+    const text_boxes = document.getElementsByClassName("nav-tabs");
+    for (const textBox of text_boxes) {
+        textBox.style.color = "black";
+    }
+    themeBG.style.backgroundColor = "#212F3D"
+    themeBG.style.borderRadius = "2em"
+    themeBG.src = LastIcon;
+    themeBG.alt = "lasticon"
+}
+
+themeBG.addEventListener("click", () => {
+    if (themeOn === false) {
+        Darktheme();
+        themeOn = true;
+    } else {
+        Lighttheme();
+        themeOn = false;
+    }
+})
